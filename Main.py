678cfc4225fa5ed49f30dce8cc3b3f8d86d7a23f
Nick Henderson <114141258+nickrhenderson@users.noreply.github.com>
2024-03-12 20:48:50 -0500

    Basic Deck Creation

--- Main.py
+++ Main.py
@@ -1,29 +1,15 @@
 import random
 
+# Basic Cards
+faces = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']
+suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
 
-class Card:
-    def __init__(self, suit, value):
-        self.suit = suit
-        self.value = value
-    
-    def __str__(self):
-        return f"{self.value} of {self.suit}"
-    
+# Basic Deck
+deck = []
 
-class Deck:
-    def __init__(self):
-        self.cards = []
-        for suit in ["Hearts", "Diamonds", "Clubs", "Spades"]:
-            for value in range(1, 14):
-                self.cards.append(Card(suit, value))
+# Generate Basic Deck of Cards
+for suit in suits:
+    for face in faces:
+        deck.append((face, suit))
 
-    def shuffle(self):
-        random.shuffle(self.cards)
-
-    def draw(self):
-        return self.cards.pop()
-
-deck = Deck()
-deck.shuffle()
-card = deck.draw()
-print(card)
+print(deck)
