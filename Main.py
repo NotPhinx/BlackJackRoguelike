010f7d1ba2c52e56e1c7c4388991a7b0b0de6c0c
Nick Henderson <114141258+nickrhenderson@users.noreply.github.com>
2024-03-12 21:09:08 -0500

    Game Logic

--- Main.py
+++ Main.py
@@ -1,10 +1,8 @@
 import random
 
 # Basic Cards
-faces = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']
+faces = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
-
-# Basic Deck
 deck = []
 
 # Generate Basic Deck of Cards
@@ -12,4 +10,29 @@ for suit in suits:
     for face in faces:
         deck.append((face, suit))
 
-print(deck)
+# Shuffle Newly Created Deck
+random.shuffle(deck)
+
+# Hands for player and dealer
+player = []
+dealer = []
+
+# Deal Cards
+player.append(deck.pop())
+dealer.append(deck.pop())
+player.append(deck.pop())
+dealer.append(deck.pop())
+
+player_total = (player[0][0] + player[1][0])
+dealer_total = (dealer[0][0] + dealer[1][0])
+
+# Game Logic
+if dealer_total <= 16:
+    drawn_card = deck.pop()
+    dealer.append(drawn_card)
+    dealer_total = ()
+    print('Dealer Draws a', drawn_card[0], 'of', drawn_card[1])
+
+# See Who Won   
+
+print(player_total, dealer_total)
