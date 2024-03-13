b0182298bdf21566e0dd3e7532a07fb94bf5162d
NotPhinx <129805325+NotPhinx@users.noreply.github.com>
2024-03-12 21:22:11 -0500

    Added dealer logic and game outcome logic along with a function to count the total value of cards in a hand

--- Main.py
+++ Main.py
@@ -26,13 +26,35 @@ dealer.append(deck.pop())
 player_total = (player[0][0] + player[1][0])
 dealer_total = (dealer[0][0] + dealer[1][0])
 
+def CountCards(cards):
+    listtotal = 0
+    for card in cards:
+        listtotal += card[0]
+
+    return listtotal 
+    
+        
+
+
 # Game Logic
 if dealer_total <= 16:
     drawn_card = deck.pop()
     dealer.append(drawn_card)
-    dealer_total = ()
+    print(dealer_total)
+    dealer_total = CountCards(dealer)
     print('Dealer Draws a', drawn_card[0], 'of', drawn_card[1])
 
-# See Who Won   
+# See Who Won
+if player_total > 21:
+    print('Player Busts')
+elif dealer_total > 21:
+    print('Dealer Busts')
+elif player_total > dealer_total:
+    print('Player Wins')
+elif dealer_total > player_total:
+    print('Dealer Wins')
+else:
+    print('Push')
 
+# Print Totals   
 print(player_total, dealer_total)
